d1cf571ef201f963e46291be94140dbd26192818
mrsunshines <k.hubertjankowski@gmail.com>
2017-11-07 02:01:07 +0100

    misja DOM

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var buttonContentList = document.getElementsByClassName('button');
+console.log(buttonContentList);
+var x = buttonContentList.length;
+console.log(x);
+for (var i = 0; i <= x; i++) {
+	var y = document.getElementsByTagName("BUTTON")[i].textContent;
+	alert(y);
+}
