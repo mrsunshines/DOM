f458c950ee9cd7a22f53fb5a9643103d4cfc40c4
mrsunshines <k.hubertjankowski@gmail.com>
2017-11-07 02:18:51 +0100

    mala korekta

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var buttonContentList = document.getElementsByClassName('button');
 console.log(buttonContentList);
 var x = buttonContentList.length;
 console.log(x);
-for (var i = 0; i <= x; i++) {
+for (var i = 0; i < x; i++) {
 	var y = document.getElementsByTagName("BUTTON")[i].textContent;
 	alert(y);
 }
